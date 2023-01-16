9636a81f264d8676c5565b775ab2e6db541b2007
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-01-17 00:20:23 +0900

    기본 엔티티/레포지토리 삽입

---
+++ src/main/java/studymongodbnosql/springmongo/domain/Customer.java
@@ -0,0 +1,46 @@
+package studymongodbnosql.springmongo.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "customer")
+public class Customer {
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
